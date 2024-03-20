44fe574dd8819d8399b1b03505baf45518a400a3
yitingliu <yiting.liu@sia-partners.com>
2024-03-20 14:15:44 -0400

    testing manager works in connecting the scene
    but not really loading the new scene

--- Assets/TestingManager.cs
+++ Assets/TestingManager.cs
@@ -1,26 +1,53 @@
+using Fusion;
 using Fusion.Addons.ConnectionManagerAddon;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class TestingManager : MonoBehaviour
 {
     private ConnectionManager connectionManager;
-    public KeyCode connectionKey; 
+    [SerializeField] InputAction connectionAction;
 
+    private void Awake()
+    {
+        connectionManager = GameObject.FindObjectOfType<ConnectionManager>();
+
+        connectionAction = new InputAction(binding: "<keyboard>/space");
+        connectionAction.Enable();
+
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(connectionKey))
+        if (connectionAction.triggered)
         {
-           _= UpdateAsync();
+            _ = UpdateAsync();
 
         }
     }
+
+    void OnDestroy()
+    {
+        connectionAction.Disable();
+
+    }
+
     async Task UpdateAsync()
     {
         Debug.Log("update async");
         await connectionManager.Connect();
+        connectionManager.onWillConnect.AddListener(LoadNewScene);
+
+    }
+    private void LoadNewScene()
+    {
+        Debug.Log("load new scene " + SceneRef.FromIndex(1));
+        connectionManager.runner.LoadScene(SceneRef.FromIndex(1), LoadSceneMode.Single);
 
     }
 }
