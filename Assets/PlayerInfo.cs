0dbb62de2c567c1178001abd5e9074ef391f1b15
yitingliu <yiting.liu@sia-partners.com>
2024-03-27 12:27:32 -0400

    playerinfo works with the loading of the information for user name

--- Assets/PlayerInfo.cs
+++ Assets/PlayerInfo.cs
@@ -3,38 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Fusion;
 
-public class PlayerInfo : MonoBehaviour
+public class PlayerInfo : NetworkBehaviour
 {
     string userNameTitle = "userName";
     string playerName;
     public TextMeshProUGUI nameText;
-    public delegate void onPlayerSet(string name);
-    public event onPlayerSet OnPlayerSet;
+/*    public delegate void onPlayerSet(string name);
+    public event onPlayerSet OnPlayerSet;*/
 
     public delegate void onPlayerGet();
     public event onPlayerGet OnPlayerGet;
 
-    public void InvokePlayerSet(string _name) {
+    /*   public void InvokePlayerSet(string _name) {
 
-        OnPlayerSet?.Invoke(_name);
-    }
+           OnPlayerSet?.Invoke(_name);
+       }
+       /// <summary>
+       /// Set Player Info such as name and avatar skin when hitting the button 
+       /// </summary>
+       public void SetPlayerInfo(string _name)
+       {
+           Debug.Log("set player info " + _name);
+           PlayerPrefs.SetString(userNameTitle, _name);
+       }*/
 
-    public void InvokePlayerGet() {
+    // display the local name to network 
+    // face the other players
+    // 
 
-        OnPlayerGet?.Invoke();
-
-    }
-    /// <summary>
-    /// Set Player Info such as name and avatar skin when hitting the button 
-    /// </summary>
-    public void SetPlayerInfo(string _name)
-    {
-        Debug.Log("set player info " + _name);
-        PlayerPrefs.SetString(userNameTitle, _name);
-    }
-
-    public void DisplayUserName(string _name)
+    [Rpc]
+    public void RPC_DisplayUserName(string _name)
     {
         nameText.text = _name;
     }
@@ -45,21 +45,30 @@ public class PlayerInfo : MonoBehaviour
     {
         Debug.Log("get player info " + userNameTitle);
         playerName = PlayerPrefs.GetString(userNameTitle);
-        DisplayUserName(playerName);
+        Debug.Log($"playerName {playerName}");
+        RPC_DisplayUserName(playerName);
     }
 
-    void Start()
+    public override void Spawned()
     {
-        DontDestroyOnLoad(gameObject);
-        OnPlayerSet += SetPlayerInfo;
-        OnPlayerGet += GetPlayerInfo;
+        OnPlayerGet?.Invoke();
+
     }
 
+    private void Awake()
+    {
+        OnPlayerGet += GetPlayerInfo;
+
+    }
     private void OnDestroy()
     {
-
-        OnPlayerSet -= SetPlayerInfo;
         OnPlayerGet -= GetPlayerInfo;
+
     }
+    public override void Despawned(NetworkRunner runner, bool hasState)
+    {
+        base.Despawned(runner, hasState);
+    }
+
 
 }
--- Assets/Scripts/TestingManager.cs
+++ Assets/Scripts/TestingManager.cs
@@ -1,30 +1,29 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using TMPro;
 
 public class TestingManager : MonoBehaviour
 {
     [SerializeField] InputAction connectionAction;
-    [SerializeField] PlayerInfo playerInfo;
-    [SerializeField] InputField inputField;
+    [SerializeField] TMP_InputField inputField;
+    string userNameTitle = "userName";
     private void Awake()
     {
-
         connectionAction = new InputAction(binding: "<keyboard>/space");
         connectionAction.Enable();
         for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
         {
             Debug.Log($"scene at {i} is {SceneManager.GetSceneByBuildIndex(i).name}");
         }
-
     }
+
     private void Update()
     {
         if (connectionAction.triggered)
         {
+            SetUserNameInOffline();
             LoadNewScene();
-
         }
     }
 
@@ -33,16 +32,14 @@ public class TestingManager : MonoBehaviour
         connectionAction.Disable();
     }
 
+    public void SetUserNameInOffline()
+    {
+
+        PlayerPrefs.SetString(userNameTitle, inputField.text);
+
+    }
     public void LoadNewScene()
     {
-        /*        Debug.Log("scene ref from index 1 isvalid" + SceneRef.FromIndex(1).IsValid);
-                Debug.Log("load new scene " + SceneRef.FromIndex(1));
-        */
-
-        playerInfo.InvokePlayerSet(inputField.text);
         SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
-
-        // connectionManager.runner.LoadScene(SceneRef.FromIndex(1), LoadSceneMode.Single);
-
     }
 }
